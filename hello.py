1eee5295f717e1503bc0be10758759e5dbdf8b03
JDeeCodes <320233136+JDeeCodes@users.noreply.github.com>
2026-09-06 22:20:33 -0500

    First Python Program and GitHub

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello")
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+favorite_color = input("What is your favorite color? ")
+print("Your favorite color is " + favorite_color + ".")
