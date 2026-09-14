3ad3edc3c0fa0dfb6f19598226f12ac15d4515e0
JDeeCodes <320233136+JDeeCodes@users.noreply.github.com>
2026-09-13 20:36:27 -0500

    Add data types assignment

---
+++ assignment2.py
@@ -0,0 +1,12 @@
+game = "Path of exile 2"
+steam_games_owned = 100
+hours_played_daily = 4.5
+I_like_video_games = True 
+print(game)
+print(steam_games_owned)
+print(hours_played_daily)
+print(I_like_video_games)
+print(type(game))
+print(type(steam_games_owned))
+print(type(hours_played_daily))
+print(type(I_like_video_games)) 
